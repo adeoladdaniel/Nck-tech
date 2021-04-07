9eb24354f4977fbae30db3b904ec540979b68821
adeola-addison <adeolaaddison@gmail.com>
2021-04-07 11:13:55 +0100

    architecure and styleguide

---
+++ build/precache-manifest.790fbc2a6dd2c89f90dee0af18673c16.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9a2725b777a9caf478caaeda590d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c49e28b98ba97ae191d",
+    "url": "/static/css/main.1af138c7.chunk.css"
+  },
+  {
+    "revision": "8a911889e06b1169ce73",
+    "url": "/static/js/2.1f20605c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.1f20605c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091584d1c3e2c507ba68",
+    "url": "/static/js/3.efe017d8.chunk.js"
+  },
+  {
+    "revision": "fe07165234709e61e0cdc05d4056de5c",
+    "url": "/static/js/3.efe017d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6694cf7a909a77508c3b",
+    "url": "/static/js/4.bc66d964.chunk.js"
+  },
+  {
+    "revision": "feeee4ed71ebcdfeefd0",
+    "url": "/static/js/5.adaf76e6.chunk.js"
+  },
+  {
+    "revision": "8c49e28b98ba97ae191d",
+    "url": "/static/js/main.331c8cb9.chunk.js"
+  },
+  {
+    "revision": "4695b36045ecfc2ed418",
+    "url": "/static/js/runtime-main.efd1af2f.js"
+  },
+  {
+    "revision": "a900d62f5f7ec3359327c942745d1f57",
+    "url": "/static/media/GoogleSans-Bold.a900d62f.woff2"
+  },
+  {
+    "revision": "00edbcf22188ce19b4f7b026955ea6bd",
+    "url": "/static/media/GoogleSans-Medium.00edbcf2.woff2"
+  },
+  {
+    "revision": "1d58c608b0bd0e8f8485ad30d654246b",
+    "url": "/static/media/GoogleSans-Regular.1d58c608.woff2"
+  }
+]);
